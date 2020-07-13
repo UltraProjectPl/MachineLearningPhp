718c37c2497cba35d752dbd2aef3d0aa18daf7fb
UltraProjectPl <jakub.sumara09@gmail.com>
2020-07-13 21:50:24 +0200

    Fixed information data, return json and load data form file

--- index.php
+++ index.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 require_once('vendor/autoload.php');
 
-$content = file_get_contents('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+
+//$content = file_get_contents('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data');
+$content = file_get_contents('./iris.csv');
 $rows = explode("\n", $content);
 
 $data = [];
 
 foreach ($rows as $row) {
+    if ('' === $row) {
+        continue;
+    }
+
     $data[] = str_getcsv($row);
 }
 
@@ -17,5 +25,10 @@ foreach ($rows as $row) {
 $specificDataTypes = array_column(array_slice($data,0, 100), 4);
 $specificDataTypes = array_map(fn (string $specie) => $specie === 'Iris-versicolor' ? 1 : -1, $specificDataTypes);
 
-$informationData =  array_slice($data, 100);
+$informationData =  array_slice($data, 0, 100);
 $informationData = array_map(null, array_column($informationData, 0), array_column($informationData, 2));
+
+echo json_encode([
+    'specificDataTypes' => $specificDataTypes,
+    'informationData' => $informationData
+]);
