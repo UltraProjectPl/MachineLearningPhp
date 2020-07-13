d6af7b8e8da4fa6560eff5daf42c4e1bad2ed574
UltraProjectPl <jakub.sumara09@gmail.com>
2020-07-13 21:21:24 +0200

    Created view

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
+    }
+};
